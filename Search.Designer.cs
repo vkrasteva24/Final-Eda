133db73c138241764f1c59b032c923e6ed08f149
vkrasteva24 <125798182+vkrasteva24@users.noreply.github.com>
2024-04-22 13:04:06 +0300

    Add files via upload

---
+++ Search.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace FinalEda
+{
+    partial class Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            search_box = new TextBox();
+            category = new ComboBox();
+            search_button = new PictureBox();
+            hi_user = new Label();
+            dataGridView1 = new DataGridView();
+            movieID1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            movieID2 = new TextBox();
+            add_watchlist = new Button();
+            add_watched = new Button();
+            go_watchlist = new Button();
+            go_watched = new Button();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)search_button).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // search_box
+            // 
+            search_box.Location = new Point(26, 92);
+            search_box.Name = "search_box";
+            search_box.PlaceholderText = "Search...";
+            search_box.Size = new Size(194, 23);
+            search_box.TabIndex = 0;
+            // 
+            // category
+            // 
+            category.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            category.ForeColor = Color.FromArgb(64, 64, 64);
+            category.FormattingEnabled = true;
+            category.Items.AddRange(new object[] { "title", "genre", "director", "language" });
+            category.Location = new Point(217, 92);
+            category.Name = "category";
+            category.Size = new Size(92, 23);
+            category.TabIndex = 1;
+            category.Text = "Category";
+            // 
+            // search_button
+            // 
+            search_button.BackColor = Color.FromArgb(62, 137, 207);
+            search_button.BackgroundImage = Properties.Resources._1965ab7ef7de87fe748b3c66efbfc73a_removebg_preview__1_;
+            search_button.Location = new Point(308, 92);
+            search_button.Name = "search_button";
+            search_button.Size = new Size(24, 23);
+            search_button.TabIndex = 2;
+            search_button.TabStop = false;
+            search_button.Click += search_button_Click;
+            // 
+            // hi_user
+            // 
+            hi_user.AutoSize = true;
+            hi_user.BackColor = Color.Transparent;
+            hi_user.Font = new Font("Gadugi", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            hi_user.ForeColor = Color.FromArgb(203, 212, 242);
+            hi_user.Location = new Point(186, 53);
+            hi_user.Name = "hi_user";
+            hi_user.Size = new Size(0, 19);
+            hi_user.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.GridColor = Color.FromArgb(52, 71, 105);
+            dataGridView1.Location = new Point(26, 131);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(306, 374);
+            dataGridView1.TabIndex = 4;
+            // 
+            // movieID1
+            // 
+            movieID1.Location = new Point(26, 542);
+            movieID1.Name = "movieID1";
+            movieID1.PlaceholderText = "Movie ID...";
+            movieID1.Size = new Size(136, 23);
+            movieID1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Gadugi", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(203, 212, 242);
+            label1.Location = new Point(26, 524);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(136, 16);
+            label1.TabIndex = 6;
+            label1.Text = "Add movie to Watchlist?";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Gadugi", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(203, 212, 242);
+            label2.Location = new Point(197, 524);
+            label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.No;
+            label2.Size = new Size(134, 16);
+            label2.TabIndex = 8;
+            label2.Text = "Add movie to Watched?";
+            // 
+            // movieID2
+            // 
+            movieID2.Location = new Point(196, 542);
+            movieID2.Name = "movieID2";
+            movieID2.PlaceholderText = "Movie ID...";
+            movieID2.Size = new Size(136, 23);
+            movieID2.TabIndex = 7;
+            // 
+            // add_watchlist
+            // 
+            add_watchlist.BackColor = Color.FromArgb(62, 137, 207);
+            add_watchlist.FlatAppearance.BorderColor = Color.FromArgb(62, 137, 207);
+            add_watchlist.FlatStyle = FlatStyle.Flat;
+            add_watchlist.ForeColor = SystemColors.ButtonHighlight;
+            add_watchlist.Location = new Point(26, 572);
+            add_watchlist.Name = "add_watchlist";
+            add_watchlist.Size = new Size(136, 23);
+            add_watchlist.TabIndex = 9;
+            add_watchlist.Text = "Add";
+            add_watchlist.UseVisualStyleBackColor = false;
+            add_watchlist.Click += add_watchlist_Click;
+            // 
+            // add_watched
+            // 
+            add_watched.BackColor = Color.FromArgb(62, 137, 207);
+            add_watched.FlatAppearance.BorderColor = Color.FromArgb(62, 137, 207);
+            add_watched.FlatStyle = FlatStyle.Flat;
+            add_watched.ForeColor = SystemColors.ButtonHighlight;
+            add_watched.Location = new Point(196, 571);
+            add_watched.Name = "add_watched";
+            add_watched.Size = new Size(136, 23);
+            add_watched.TabIndex = 10;
+            add_watched.Text = "Add";
+            add_watched.UseVisualStyleBackColor = false;
+            add_watched.Click += add_watched_Click;
+            // 
+            // go_watchlist
+            // 
+            go_watchlist.BackColor = Color.FromArgb(67, 137, 207);
+            go_watchlist.FlatAppearance.BorderColor = Color.FromArgb(67, 137, 207);
+            go_watchlist.FlatStyle = FlatStyle.Flat;
+            go_watchlist.Font = new Font("Gadugi", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            go_watchlist.ForeColor = SystemColors.ButtonHighlight;
+            go_watchlist.Location = new Point(26, 656);
+            go_watchlist.Name = "go_watchlist";
+            go_watchlist.Size = new Size(306, 37);
+            go_watchlist.TabIndex = 11;
+            go_watchlist.Text = "Watchlist";
+            go_watchlist.UseVisualStyleBackColor = false;
+            go_watchlist.Click += go_watchlist_Click;
+            // 
+            // go_watched
+            // 
+            go_watched.BackColor = Color.FromArgb(67, 137, 207);
+            go_watched.FlatAppearance.BorderColor = Color.FromArgb(67, 137, 207);
+            go_watched.FlatStyle = FlatStyle.Flat;
+            go_watched.Font = new Font("Gadugi", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            go_watched.ForeColor = SystemColors.ButtonHighlight;
+            go_watched.Location = new Point(26, 711);
+            go_watched.Name = "go_watched";
+            go_watched.Size = new Size(306, 37);
+            go_watched.TabIndex = 12;
+            go_watched.Text = "Watched";
+            go_watched.UseVisualStyleBackColor = false;
+            go_watched.Click += go_watched_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Gadugi", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(203, 212, 242);
+            label3.Location = new Point(23, 630);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 22);
+            label3.TabIndex = 13;
+            label3.Text = "Check Out:";
+            // 
+            // Search
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.IMG_7406;
+            ClientSize = new Size(357, 776);
+            Controls.Add(label3);
+            Controls.Add(go_watched);
+            Controls.Add(go_watchlist);
+            Controls.Add(add_watched);
+            Controls.Add(add_watchlist);
+            Controls.Add(label2);
+            Controls.Add(movieID2);
+            Controls.Add(label1);
+            Controls.Add(movieID1);
+            Controls.Add(dataGridView1);
+            Controls.Add(hi_user);
+            Controls.Add(search_button);
+            Controls.Add(category);
+            Controls.Add(search_box);
+            Name = "Search";
+            Text = "Search";
+            ((System.ComponentModel.ISupportInitialize)search_button).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox search_box;
+        private ComboBox category;
+        private PictureBox search_button;
+        private Label hi_user;
+        private DataGridView dataGridView1;
+        private TextBox movieID1;
+        private Label label1;
+        private Label label2;
+        private TextBox movieID2;
+        private Button add_watchlist;
+        private Button add_watched;
+        private Button go_watchlist;
+        private Button go_watched;
+        private Label label3;
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,124 @@
+﻿using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.ComponentModel.DataAnnotations;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Final_Eda.Authenticate;
+using Final_Eda;
+
+namespace FinalEda
+{
+    public partial class Search : Form
+    {
+        private Users authenticatedUser;
+
+        public Search()
+        {
+            authenticatedUser = User;
+            InitializeComponent();
+
+            hi_user.Text = $"Welcome: {authenticatedUser.first_name}";
+        }
+
+        private void search_button_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(@"Data Source=LAB109PC02\SQLEXPRESS; Initial Catalog=GoodWatch; Integrated Security=True;"))
+            {
+                sqlCon.Open();
+
+                string query = "SELECT * FROM Movies WHERE 1 = 1";
+
+
+                if (category.SelectedItem != null)
+                {
+                    string query2 = "SELECT * FROM Movies WHERE @category LIKE @search_box";
+
+                    SqlCommand cmd = new SqlCommand(query2, sqlCon);
+
+                    cmd.Parameters.Add(new SqlParameter("@category", category.SelectedItem.ToString()));
+                    cmd.Parameters.Add(new SqlParameter("@search_box", search_box.Text));
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                        dataGridView1.DataSource = ds.Tables.Count > 0 ? ds.Tables[0] : null;
+                    }
+                }
+            }
+        }
+
+
+        private void add_watchlist_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(@"Data Source=LAB109PC02\SQLEXPRESS; Initial Catalog=GoodWatch; Integrated Security=True;"))
+            {
+                sqlCon.Open();
+
+                if (movieID1.Text != null)
+                {
+                    string query3 = "UPDATE Watchlist SET status = 'Watching' WHERE movieID=@movieID1";
+
+                    SqlCommand updateStatusCmd = new SqlCommand(query3, sqlCon);
+                    updateStatusCmd.Parameters.AddWithValue("@movieID1", movieID1.Text);
+                    updateStatusCmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Added Succesfully!");
+                }
+                else
+                {
+                    MessageBox.Show("Failed.");
+                }
+
+            }
+        }
+
+        private void add_watched_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(@"Data Source=LAB109PC02\SQLEXPRESS; Initial Catalog=GoodWatch; Integrated Security=True;"))
+            {
+                sqlCon.Open();
+
+                if (movieID2.Text != null)
+                {
+                    string query4 = "UPDATE Watched SET status = 'Watched' WHERE movieID=@movieID2";
+
+                    SqlCommand updateStatusCmd = new SqlCommand(query4, sqlCon);
+                    updateStatusCmd.Parameters.AddWithValue("@movieID2", movieID2.Text);
+                    updateStatusCmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Added Succesfully!");
+                }
+                else
+                {
+                    MessageBox.Show("Failed.");
+                }
+
+            }
+        }
+        private void go_watchlist_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Watchlist Watchlist = new Watchlist();
+            Watchlist.ShowDialog();
+        }
+
+        private void go_watched_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Watched Watched = new Watched();
+            Watched.ShowDialog();
+        }
+    }
+}
+
